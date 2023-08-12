9cbbe043f499a5b7ca52ff12f9028a8452d2b4dd
gustave-studio <gustave0222@gmail.com>
2023-08-13 02:54:53 +0900

    ✨自動販売機のアルゴリズム

---
+++ vending_machine/vending_machine.py
@@ -0,0 +1,25 @@
+import sys
+
+input_price = input('insert:')
+product_price = input('product:')
+change = int(input_price) - int(product_price)
+
+if not input_price.isdecimal():
+    print('整数を入力してください')
+    sys.exit()
+
+if change < 0:
+    print('整数を入力してください')
+    sys.exit()
+
+
+coin_type = [ 5000, 1000, 500, 100, 50, 10, 5, 1 ]
+
+return_coin = {}
+
+for coin in coin_type:
+    return_coin[coin] = change // coin
+    change = change % coin
+    print((f'change: {change}'))
+
+print(return_coin)
