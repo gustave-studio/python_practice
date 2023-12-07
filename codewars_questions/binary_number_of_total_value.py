8ea1e71aa246510d5a087c8caf3a1a649659a069
gustave-studio <gustave0222@gmail.com>
2023-12-07 23:44:06 +0900

    簡潔に書けるので修正

--- codewars_questions/binary_number_of_total_value.py
+++ codewars_questions/binary_number_of_total_value.py
@@ -1,18 +1,5 @@
 def arr2bin(arr):
-    for item in arr:
-        if type(item) is not int:
+    for x in arr:
+        if(type(x) != int):
             return False
-    
-    number = sum(arr)
-    
-    if number is 0:
-        return '0'
-    
-    result = ''
-    
-    while number > 0:
-        result = str(number % 2) + result
-        print(f'result: {result}')
-        number //= 2
-
-    return result
+    return '{0:b}'.format(sum(arr))
